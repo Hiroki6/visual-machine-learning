ce79eafeb8d5ffa66af04bd4ed0dc57e29eb32f4
藤野宏樹 <fujinohiroki@fujinohiroki-no-MacBook-Pro.local>
2016-06-10 02:22:29 +0900

    change directory name

---
+++ binary_classification/src/ofApp.cpp
@@ -0,0 +1,78 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetWindowShape(800, 800);
+    ofBackground(0, 0, 0);
+    ofSetFrameRate(0.001);
+    ofSetCircleResolution(64);
+    ofEnableAlphaBlending();
+    perceptron.setup();
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    perceptron.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofTranslate(400, 400);
+    perceptron.draw();
+    ofSetColor(255, 255, 255);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
